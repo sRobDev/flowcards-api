ace2cf1d81df8981dc2fdb0ae483530884af7ab3
srobDev <sprobertson94@gmail.com>
2021-06-10 09:43:05 -0600

    More routes, switched to cards, topics, user

--- server/app.js
+++ server/app.js
@@ -6,13 +6,16 @@ const router = require('@koa/router')();
 const app = module.exports = new Koa();
 const prisma = new PrismaClient();
 
-// app.use(bodyParser());
 app.use(koaBody());
-// app.use(ctx => ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`);
 
 router.get('/', hello)
-  .get('/retrieve/:id', test)
-  .post('/create', create);
+  .post('/user/new', createUser)
+  .get('/user/:id', getUser)
+  .post('/card/new/:id', createCard)
+  .post('/card/:id/update', updateCard)
+  .get('/card/:id', getCard)
+  .post('/topic/new/:id', createTopic)
+  .get('/topic/:id', getTopic);
 
 app.use(router.routes());
 
@@ -21,23 +24,55 @@ async function hello(ctx) {
   ctx.body = 'Hello World';
 }
 
-async function test(ctx) {
-  ctx.body = await prisma.test.findUnique({
-    where: {
-      id: parseInt(ctx.params.id)
+async function getUser(ctx) {
+  ctx.body = await prisma.user.findUnique({ where: { id: parseInt(ctx.params.id) } })
+}
+
+async function createUser(ctx) {
+  ctx.body = await prisma.user.create({
+    data: {
+      name: ctx.request.body.name
     }
   });
 }
 
-async function create(ctx) {
-  if(!ctx.request.body) ctx.throw(400, 'Bad Request!');
-  const { comment } = ctx.request.body;
-
-  const newComment = await prisma.test.create({
-    data: { comment }
-  });
-
-  ctx.body = newComment;
+async function getCard(ctx) {
+  ctx.body = await prisma.card.findUnique({ where: { id: parseInt(ctx.params.id) } })
 }
+
+async function createCard(ctx) {
+  const authorId = +ctx.params.id;
+
+  ctx.body = await prisma.card.create({ data: {...ctx.request.body, authorId } });
+}
+
+async function updateCard(ctx) {
+  ctx.request.body.topicId = +ctx.request.body.topicId || null;
+  ctx.body = await prisma.card.update({
+    where: {
+      id: parseInt(ctx.params.id)
+    },
+    data: { ...ctx.request.body }
+  });
+}
+
+async function getTopic(ctx) {
+  ctx.body = await prisma.topic.findUnique({
+    where: {
+      id: parseInt(ctx.params.id)
+    },
+    include: {
+      cards: true
+    }
+  });
+}
+
+async function createTopic(ctx) {
+  const authorId = +ctx.params.id;
+
+  ctx.body = await prisma.topic.create({ data: {...ctx.request.body, authorId } });
+}
+
+const simpleCreate = async (body, table) => await prisma[table].create({ data: {...body } });
 
 if (!module.parent) app.listen(3000);
---
+++ server/prisma/migrations/20210610144011_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Topic" ALTER COLUMN "description" DROP NOT NULL;
